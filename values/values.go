3cd462cd4a2d403ec1a364505021e537b6aac365
Vanguard90 <enesmil@gmail.com>
2023-11-20 21:18:35 +0100

    Values example

---
+++ values/values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("go" + "lang") // String concatenation
+
+    fmt.Println("1+1 =", 1+1) // Integer
+    fmt.Println("7.0/3.0 =", 7.0/3.0) // Float
+
+    fmt.Println(true && false) // Booleans
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
